4029db7568f8318dd4da0d01f756ffd72da9b7c4
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-05-18 09:59:07 +0200

    Adjusted felx box

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import { Platform, StyleSheet, View, TextInput } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -45,18 +45,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    justifyContent: 'flex-start',
   },
 });

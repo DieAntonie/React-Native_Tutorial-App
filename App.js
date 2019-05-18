8681dcefb6518626646a660d78db498a0bc45c83
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-05-18 10:43:25 +0200

    positioning elements with flex box

--- App.js
+++ App.js
@@ -7,41 +7,56 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
 
-  state = {
-    placeName: "",
-  };
+    state = {
+        placeName: "",
+    };
 
-  placeNameChangedHandler = (text) => {
-    this.setState({
-      placeName: text
-    })
-  };
+    placeNameChangedHandler = (text) => {
+        this.setState({
+            placeName: text
+        })
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          placeholder="An awesome place"
-          onChangeText={this.placeNameChangedHandler}
-          value={this.state.placeName}
-        />
-        <Button title="Add" />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.placeInput}
+                        placeholder="An awesome place"
+                        onChangeText={this.placeNameChangedHandler}
+                        value={this.state.placeName} />
+                    <Button
+                        style={styles.placeButton}
+                        title="Add" />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    justifyContent: 'flex-start',
-  },
+    container: {
+        flex: 1,
+        padding: 20,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        justifyContent: 'flex-start',
+    },
+    inputContainer: {
+        alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    placeInput: {
+        width: "70%",
+    },
+    placeButton: {
+        width: "30%",
+    }
 });

8309848093a5cff097dc05bea3f4a7343252a819
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-06-23 10:49:26 +0200

    Implement initial place reducer

---
+++ src/store/reducers/places.js
@@ -0,0 +1,15 @@
+const initialState = {
+    selectedPlace: null,
+    places: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case value:
+            break;
+        default:
+            return state;
+    }
+}
+
+export default reducer;
